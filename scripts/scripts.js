569605bab40a20a3dba92608745ad014f074c306
Kamariasullins <144828932+Kamariasullins@users.noreply.github.com>
2024-01-18 11:42:02 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,55 +1,6 @@
-  
 
-
-
-
-
-//Global variables
-var video = document.getElementById("example");
-var videoSource = document.getElementById("vid-src");
-var descriptionSource = document.getElementById("despsrc");
-
-  //Hamburger menu function
-  function hamburger() {
-    var menu = document.getElementById("menu-links");
-    var logo = document.getElementById("ffc-logo");
-    if (menu.style.display === "block" && logo.style.display === "none"){
-        menu.style.display = "none";
-        logo.style.display = "block";
-     } else{
-        menu.style.display = "block";
-        logo.style.display = "none";
-     }
-  }
-
-  //fuction to display burpess example video
-  function burpees() {
-   videoSource.src = "media/burpees.mp4";
-   descriptionSource.src ="media/burpees-descriptions.vtt";
-   video.style.display = "block";
-   video.load();
-  }
-
-  //function to display the plank example video
-  function plank() {
-    videoSource.src = "media/plank.mp4";
-    descriptionSource.src ="media/plank-descriptions.vtt";
-    video.style.display="block";
-    video.load();
-  }
-
-  //function to display the mountain climbers example video 
-  function mountain(){
-   videoSource.src = "media/mc.mp4"
-   descriptionSource.src="media/mountain-descriptions.vtt"
-   video.style.display ="block";
-   video.load();
-  }
-
-  //function to display a promo code
-  function discount() {
-    var promo = document.getElementById("special");
-    promo.firstChild.nodeValue = "Promo Code: D25START";
-    promo.style.color="#ff0000";
-    promo.style.fontSize = "2em";
-  }
+document.querySelector('.hamburger-button').addEventListener('click', function() {
+    document.querySelector('.nav-links').style.display = 
+    (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
+ });
+ 
