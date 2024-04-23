0880ceb70db73ada9d0c8dadae7f7e4ba3a1644c
Kamariasullins <144828932+Kamariasullins@users.noreply.github.com>
2024-04-23 13:36:41 -0400

    Add files via upload

---
+++ Chapter01/js01/project04/project01-04.js
@@ -0,0 +1,8 @@
+var homeStreet = "1 Main St.";
+var homeCity = "Sicilia";
+var homeState = "MA";
+var homeCode = "02103";
+var workStreet = "15 Oak Ln";
+var workCity = "Central City";
+var workState = "MA";
+var workCode = "02104";
---
+++ Chapter02/js02/project02/project02-02_txt.js
@@ -0,0 +1,35 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: 
+      Date:   
+
+      Filename: project02-02.js
+ */
+ 
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: kamaria sullins
+      Date:  4/23/2024
+
+      Filename: project02-02.js
+ */
+ 
+//Function to verify form
+function verifyForm() {
+      let name = document.getElementById("name").value;
+      let email = document.getElementById("email").value;
+      let phone = document.getElementById("phone").value;
+      (document.getElementById("name").value !== "") && (document.getElementById("email").value !== "") && 
+      (document.getElementById("phone").value !== "")
+      ? window.alert("Thank you!") :
+     window.alert("Please fill in all fields");
+  }
+     //Event Listener
+     document.getElementById("submit").onclick = verifyForm;
+     
